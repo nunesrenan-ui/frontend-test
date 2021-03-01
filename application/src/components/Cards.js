3d97893cab43c639c24bd12a814156b073fb02ff
Leonardo <leozikasantos73@gmail.com>
2021-03-01 15:48:10 -0300

    finishing the Cards component

--- application/src/components/Cards.js
+++ application/src/components/Cards.js
@@ -7,7 +7,10 @@ const Cards = () => {
             <div className="container">
                 <span>Encontrados 20 herois</span>
                 <div className="wrapper">
+                    <i><img src="./images/superhero.png" width="20" /></i> 
                     <span>Ordenar por nome - A/Z</span>
+                    <img src="./images/toggle-left.png" width='70' />
+                    <img src="./images/hearth-1.png" width="20"/>
                     <span>Somente Favoritos</span>
                 </div>
             </div>
